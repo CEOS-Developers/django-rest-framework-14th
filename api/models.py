c54fdcd292be56a25594c5a016bc44d7db35a88f
Min A <min2010011@gmail.com>
2021-10-07 05:28:50 +0900

    Modify models

--- api/models.py
+++ api/models.py
@@ -4,21 +4,21 @@ from django.db import models
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=20, blank=True)
+    name = models.CharField(max_length=20)
     bio = models.CharField(max_length=100, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos', null=True, blank=True)
 
     def __str__(self):
-        return 'user: ' + self.username
+        return self.name
 
 
 class Post(models.Model):
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     caption = models.TextField(max_length=300)
 
     def __str__(self):
-        return 'post: ' + self.id + ', author: ' + self.author.username + ', at: ' + self.created_at
+        return f'{self.caption} created by {self.author.username}'
 
 
 class Video(models.Model):
@@ -26,7 +26,7 @@ class Video(models.Model):
     video_url = models.FileField(upload_to='videos')
 
     def __str__(self):
-        return 'video: ' + self.id + ', post: ' + self.post.id
+        return f'{self.id} of post_id: {self.post.id}'
 
 
 class Image(models.Model):
@@ -34,22 +34,22 @@ class Image(models.Model):
     image_url = models.FileField(upload_to='images')
 
     def __str__(self):
-        return 'image: ' + self.id + ', post: ' + self.post.id
+        return f'{self.id} of post_id: {self.post.id}'
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=100);
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return 'comment: ' + self.content + ', author: ' + self.author.username
+        return f'{self.content} commented by {self.author.username}'
 
 
 class Like(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.author.username + ' likes ' + self.post.id
+        return f'{self.author.username} likes post: {self.post}'
