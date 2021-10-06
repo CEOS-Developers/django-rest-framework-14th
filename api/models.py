1ece6ffe5d634876de216730867ad149d3648bd0
chaeri93 <chaeri93@ewhain.net>
2021-10-06 22:11:01 +0900

    add Profile Model

--- api/models.py
+++ api/models.py
@@ -11,4 +11,13 @@ class User(AbstractBaseUser):  # AbstractBaseUser를 상속받고 있는 모든 
     tel = models.CharField("연락처", max_length=20, null=True, blank=True)
 
 
+class Profile(models.Model):  # 유저의 세부 정보를 담는 모델
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)  # User모델과 1:1 로 관계
+    nickname = models.CharField(max_length=40, blank=True)
+    introduction = models.TextField(blank=True)  # 인스타 프로필 소개
+    image = models.TextField()  # 인스타 사진
+    # profile_name = models.TextField()  # 인스타에 표시한 이름
+
+
+
 
