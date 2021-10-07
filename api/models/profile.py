94ec3ae747fda42115e129f030f2fcd5eb1ff52f
junhwan <ujunhwan@gmail.com>
2021-10-07 12:49:39 +0900

    change migrations

--- api/models/profile.py
+++ api/models/profile.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
-class Member(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     nickname = models.TextField(max_length=15)
     private = models.BooleanField(default=False)
@@ -14,3 +12,4 @@ class Member(models.Model):
 
     def __str__(self):
         return self.nickname
+
