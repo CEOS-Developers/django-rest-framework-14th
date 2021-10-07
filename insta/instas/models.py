00d305286b342a49dcf32ad0d0317914d5223088
hwanu <sossont1@gmail.com>
2021-10-08 01:17:48 +0900

    모델 수정

--- insta/instas/models.py
+++ insta/instas/models.py
@@ -8,6 +8,8 @@ class Users(models.Model):
     user_id = models.CharField(max_length=20, primary_key=True)
     user_pw = models.CharField(max_length=20)
     email =  models.EmailField()
+    def __str__(self):
+        return self.uid
 
 class Profile(models.Model):
     uid = models.AutoField(primary_key=True)
@@ -17,34 +19,44 @@ class Profile(models.Model):
     introduction = models.TextField()
     phone_num = models.IntegerField()
     gender = models.CharField(max_length=6)
-    post_id = models.IntegerField()
     followers = models.IntegerField()
     following = models.IntegerField()
-    story_id = models.IntegerField()
+
+    def __str__(self):
+        return self.user_name
 
 class Posts(models.Model):
     uid = models.ForeignKey(Users,on_delete=models.CASCADE)
     post_id = models.AutoField(primary_key=True)
     user_id = models.CharField(max_length=20)
     location = models.CharField(max_length=30)
+    title = models.TextField()
     email = models.EmailField()
     comments = models.TextField()
     likes = models.IntegerField()
-    files_id = models.IntegerField()
+
+    def __str__(self):
+        return self.title
 
 class Photos(models.Model):
     photo_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(Posts,on_delete=models.CASCADE)
     post_id = models.ForeignKey(Posts,on_delete=models.CASCADE)
-    photo_url = models.FileField()
+    photo_url = models.ImageField(upload_to="post/Photos")
     date = models.DateTimeField()
+
+    def __str__(self):
+        return self.photo_id
 
 class Videos(models.Model):
     video_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(Posts,on_delete=models.CASCADE)
     post_id = models.ForeignKey(Posts,on_delete=models.CASCADE)
-    video_url = models.FileField()
+    video_url = models.FileField(upload_to="post/Videos")
     date = models.DateTimeField()
+
+    def __str__(self):
+        return self.video_id
 
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
@@ -52,8 +64,14 @@ class Comment(models.Model):
     post_id = models.ForeignKey(Posts,on_delete=models.CASCADE)
     comment = models.TextField()
 
+    def __str__(self):
+        return self.comment
+
 class Story(models.Model):
     uid = models.ForeignKey(Users,on_delete=models.CASCADE)
-    story_id = models.AutoField()
+    story_id = models.AutoField(primary_key=True)
     date = models.DateTimeField()
+
+    def __str__(self):
+        return self.story_id
 
