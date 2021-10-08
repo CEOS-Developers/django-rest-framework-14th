199a72175a191dd7aa01adbad092e9377cb79364
hywn <14hhan@gmail.com>
2021-10-08 16:02:19 +0900

    refactor: FollowRelation 추가 및 모델 코드 리팩토링

--- api/models.py
+++ api/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.core.validators import FileExtensionValidator
+from django.core.validators import FileExtensionValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     account_name = models.TextField(max_length=30)
     phone = models.TextField(max_length=20)
-    bio = models.TextField(max_length=150)
-    # profile_photo = models.ImageField(blank=True, upload_to='profiles')
+    bio = models.TextField(max_length=150, blank=True)
+    profile_photo = models.ImageField(blank=True, upload_to='profiles')
 
     def __str__(self):
         return self.account_name
@@ -16,31 +16,29 @@ class Profile(models.Model):
 
 class Post(models.Model):
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='posts')
-    description = models.CharField(max_length=255, blank=True)
+    caption = models.TextField(max_length=2200, blank=True)
     date_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.profile.account_name + "'s post: " + self.description
+        return f"{self.profile.account_name}'s post: {self.caption}"
 
 
 class Photo(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='photos')
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='photos')
-    # image_file = models.ImageField(upload_to='posts/photos')
+    image_file = models.ImageField(upload_to='posts/photos')
 
     def __str__(self):
-        return 'Photo from ' + self.profile.account_name + ': ' + self.post.description
+        return f"Photo from {self.post.profile.account_name} 's post: {self.post.caption}"
 
 
 class Video(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='videos')
-    profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='videos')
-    # video_file = models.FileField(upload_to='posts/videos', validators=[
-    #     FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])
-    # ])
+    video_file = models.FileField(upload_to='posts/videos', validators=[
+        FileExtensionValidator(allowed_extensions=['MOV', 'avi', 'mp4', 'webm', 'mkv'])
+    ])
 
     def __str__(self):
-        return 'Video from ' + self.profile.account_name + "'s post" + self.post.description
+        return f"Video from {self.post.profile.account_name} 's post: {self.post.caption}"
 
 
 class Like(models.Model):
@@ -54,6 +52,18 @@ class Like(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField(max_length=2200, blank=False)
 
     def __str__(self):
-        return self.profile + ' left a comment on ' + self.post.profile.account_name + "'s post: " + self.post.description
+        return f"{self.profile.account_name}'s comment on {self.post.profile.account_name}'s post: {self.content}"
+
+
+class FollowRelation(models.Model):
+    follower = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='follow')
+    followee = models.ForeignKey('Profile', on_delete=models.CASCADE, related_name='followed')
+
+    def __str__(self):
+        return f'{self.follower.account_name} follows {self.followee.account_name}'
+
+
+Profile.add_to_class('followings', models.ManyToManyField('self', through=FollowRelation, related_name='followers', symmetrical=False))
