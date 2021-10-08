affcf1f556803adbdf78379e516d8363c41220d2
hywn <14hhan@gmail.com>
2021-10-08 16:03:00 +0900

    chore: admin 페이지에서 새로 추가한 모델 관리 가능하도록 수정

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile, Post, Photo, Video, Like, Comment, FollowRelation
 
-# Register your models here.
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Photo)
+admin.site.register(Video)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(FollowRelation)
