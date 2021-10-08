e3def31d9d4c29402b62841fbba3530dbaaff011
hwanu <sossont1@gmail.com>
2021-10-09 03:11:12 +0900

    Likes Model 추가

--- insta/instas/models.py
+++ insta/instas/models.py
@@ -70,3 +70,9 @@ class Story(models.Model):
     def __str__(self):
         return self.story_id
 
+class Likes(models.Model):
+    post_id = models.OneToOneField(Users, on_delete=models.CASCADE)
+    author = models.OneToOneField(Posts,on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self):
+        return self.author
