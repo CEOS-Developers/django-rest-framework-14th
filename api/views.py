a3afedb19d3cf6b59acc64340b409466ff3ae735
mongus <kjyanus@gmail.com>
2021-10-12 21:49:31 +0900

    update views.py; comment

--- api/views.py
+++ api/views.py
@@ -15,11 +15,15 @@ def post_view(request):
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)  # JSONParser 객체를 생성하고 request에서 데이터 추출
-        serializer = PostSerializer(data=data)  # 추출한 데이터를 이용해서 pythonic data 만듬
+        # 추출한 데이터를 이용해서 pythonic data 만듬
+        serializer = PostSerializer(data=data)
         if serializer.is_valid():  # 데이터가 유효할 경우
-            temp_user = User.objects.get(id=1)  # (임시) 유저 1번이 작성한 게시물인 것처럼
+            # (임시) user1이 작성한 게시물인 것으로 저장
+            # 추후에는 현재 로그인한 유저정보를 request.user로 받아와서 저장하면 됨
+            temp_user = User.objects.get(id=1)
             serializer.save(user=temp_user)  # 요청받은 데이터 저장
-            return JsonResponse(serializer.data, status=201, safe=False)  # status 201 created
+            # status 201 created
+            return JsonResponse(serializer.data, status=201, safe=False)
 
         # 유효하지 않은 데이터일 경우 error 발생, status 400 bad request
         return JsonResponse(serializer.errors, status=400)
