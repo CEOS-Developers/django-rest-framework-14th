5eb7036f93accc2922eb6738428309b72c626aa8
junhwan <ujunhwan@gmail.com>
2021-10-12 09:57:12 +0900

    update User: add lookup methods

--- user/models.py
+++ user/models.py
@@ -17,6 +17,24 @@ class User(AbstractUser, Base):
     def __str__(self):
         return self.nickname
 
+    def get_all_posts(self):
+        return self.post.all()
+
+    def get_all_comments(self):
+        return self.comment.all()
+
+    def get_all_followers(self):
+        return self.follower.all()
+
+    def get_followers_count(self):
+        return self.get_all_followers().count()
+
+    def get_all_followings(self):
+        return self.following.all()
+
+    def get_followings_count(self):
+        return self.get_all_followings().count()
+
 
 class Follow(Base):
     following_user = models.ForeignKey('user', on_delete=models.CASCADE, related_name='following')
