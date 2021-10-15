dcacb7ccc5ec5f4a1380bf9659bbbf9af1fd6a77
hywn <14hhan@gmail.com>
2021-10-15 13:18:42 +0900

    refactor: Profile 모델 리팩토링
    - account_name, phone의 중복 방지
    - profile_photo에 디폴트 파일 설정
    - phone에는 숫자만 들어갈 수 있도록 설정

--- api/models.py
+++ api/models.py
@@ -7,15 +7,14 @@ from django.utils.translation import gettext_lazy as _
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    account_name = models.TextField(max_length=30)
-    phone = models.TextField(max_length=20)
+    account_name = models.CharField(max_length=30, unique=True)
+    phone = models.CharField(max_length=20, unique=True)
     bio = models.TextField(max_length=150, blank=True)
-    profile_photo = models.ImageField(blank=True, upload_to='profiles')
+    profile_photo = models.ImageField(blank=True, upload_to='profiles', default='profiles/tmp4et5jeut.jpg')
 
     def clean(self):
-        existing_account = Profile.objects.filter(account_name=self.account_name).first()
-        if existing_account != None and existing_account.id != self.id:
-            raise ValidationError(_('Account names must be unique.'))
+        if not self.phone.isdigit():
+            return ValidationError(_('Phone must be numeric only.'))
 
     def __str__(self):
         return self.account_name
