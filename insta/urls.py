8de94ee38ebd9ac42ea38b23d4b3345117956000
ssssujini99 <=>
2021-11-07 02:53:05 +0900

    insta앱 urls.py 수정

--- insta/urls.py
+++ insta/urls.py
@@ -3,13 +3,13 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('profiles/', views.profileList.as_view()),
-    path('profiles/<int:pk>/', views.profileDetail.as_view()),
+    path('profile/', views.profileList.as_view()),
+    path('profile/<int:pk>/', views.profileDetail.as_view()),
     # path('profiles/create/', views.profileCreate, name='profile-create'),
     #
-    path('posts/', views.postList.as_view()),
-    path('posts/<int:pk>/', views.postDetail.as_view()),
+    path('post/', views.postListAPIView.as_view()),
+    path('post/<int:pk>/', views.postDetailAPIView.as_view()),
     #
-    path('follows/', views.followList.as_view()),
-    path('follows/<str:pk>/', views.followDetail.as_view()),
+    path('follow/', views.followList.as_view()),
+    path('follow/<str:pk>/', views.followDetail.as_view()),
 ]
