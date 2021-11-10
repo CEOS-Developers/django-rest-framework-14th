9365fa80555a50ab166d9656fc66212b8492eb5e
YOONJI KIM <77983074+stritegdc@users.noreply.github.com>
2021-11-10 17:02:56 +0900

    Add API View to User: UserList, UserDetail

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     path('posts/', views.PostList.as_view(), name='posts'),
     path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('users/', views.UserList.as_view(), name='users'),
+    path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from rest_framework.parsers import JSONParser
 from .models import Post, User
 from .serializers import PostSerializer, UserSerializer
 from rest_framework.views import APIView
@@ -8,6 +6,7 @@ from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
 # Create your views here.
+
 
 class PostList(APIView):
     """
@@ -24,6 +23,7 @@ class PostList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class PostDetail(APIView):
     """
@@ -52,5 +52,41 @@ class PostDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetail(APIView):
+    def get_object(self, pk):
+        user = get_object_or_404(User, pk=pk)
+        return user
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_object(pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
