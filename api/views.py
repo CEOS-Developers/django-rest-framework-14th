7135424e31ab7d51ddc7e75e7897289e1130a81a
hwanu <sossont1@gmail.com>
2021-11-11 00:17:22 +0900

    PUT DELETE 구현

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import JsonResponse
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
@@ -34,3 +35,22 @@ class PostDetailView(APIView):
         serializer = PostSerializer(post_data, many=True)
         return JsonResponse(serializer.data, safe=False)
 
+    def put(self,request,post_id):
+        if post_id is None:
+            return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+        else:
+            post_object = Post.objects.get(id=post_id)
+            update_post_serializer = PostSerializer(post_object,data=JSONParser().parse(request))
+            if update_post_serializer.is_valid():
+                update_post_serializer.save()
+                return JsonResponse(update_post_serializer.data, status=201)
+            else:
+                return JsonResponse(update_post_serializer.errors, status=400)
+
+    def delete(self, request, post_id):
+        if post_id is None:
+            return Response("No Content Request", status=status.HTTP_204_NO_CONTENT)
+        else:
+            post_object = Post.objects.get(id=post_id)
+            post_object.delete()
+            return Response("Delete Success",status=status.HTTP_200_OK)
