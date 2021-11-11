25923bbeea658958c186daf0e09dcfbf16dff7cb
ssssujini99 <sssssjin99@naver.com>
2021-11-11 15:51:55 +0900

    insta/serializers.py 변경

--- insta/serializers.py
+++ insta/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-from .models import Profile, Post, Comment, Follow, FollowRelation
-from django.contrib.auth.models import User
+from .models import User, Post, Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SerializerMethodField() # serializer method field 이용
 
     def get_author(self, obj):
-        return obj.author.nickname # author의 값이 foreignkey로 pk인 id 값을 가져오기 때문에 nickname을 가져오도록 재정의
+        return obj.author.username # author의 값이 foreignkey로 pk인 id 값을 가져오기 때문에 nickname을 가져오도록 재정의
 
     class Meta:
         model = Comment
@@ -18,8 +17,8 @@ class PostListSerializer(serializers.ModelSerializer):
     comment_set = CommentSerializer(many=True, read_only=True) # Nested Serializer를 이용해 relation을 맺은 다른 모델과의 관계 표현하기
     author = serializers.SerializerMethodField() # serializer method field 이용
 
-    def get_author(self, obj): # author의 값이 foreignkey로 pk인 id 값을 가져오기 때문에 nickname을 가져오도록 재정의
-        return obj.author.nickname
+    def get_author(self, obj): # author의 값이 foreignkey로 pk인 id 값을 가져오기 때문에 username을 가져오도록 재정의
+        return obj.author.username
 
     class Meta:
         model = Post
@@ -35,44 +34,50 @@ class PostCreateSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username']
+        fields = ['id', 'username', 'email', 'created_at', 'updated_at']
 
 
-class FolloweeListingField1(serializers.RelatedField):
-    def to_representation(self, value):
-        return value.follower.nickname
-
-
-class ProfileListSerializer(serializers.ModelSerializer):
-    post_set = PostListSerializer(many=True, read_only=True)
-    user = UserSerializer(read_only=True)
-    followee = FolloweeListingField1(many=True, read_only=True) # many to many 관계 모델 기져오기
-
+class UserCreateSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Profile
-        fields = ['id', 'nickname', 'user', 'followee', 'post_set'] # followee: 역참조
+        model = User
+        fields = ['id', 'username', 'email']
 
 
-class ProfilePartSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-
-    class Meta:
-        model = Profile
-        fields = ['id', 'nickname', 'user']
-
-
-class FolloweeListingField2(serializers.RelatedField):
-    def to_representation(self, value):
-        return value.nickname
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    follower = serializers.SerializerMethodField()
-    followee = FolloweeListingField2(many=True, read_only=True)
-
-    def get_follower(self, obj):
-        return obj.follower.nickname
-
-    class Meta:
-        model = Follow
-        fields = ['follower', 'followee']
+# class FolloweeListingField1(serializers.RelatedField):
+#    def to_representation(self, value):
+#       return value.follower.nickname
+#
+#
+# class ProfileListSerializer(serializers.ModelSerializer):
+#     post_set = PostListSerializer(many=True, read_only=True)
+#     user = UserSerializer(read_only=True)
+#     followee = FolloweeListingField1(many=True, read_only=True) # many to many 관계 모델 기져오기
+#
+#     class Meta:
+#         model = Profile
+#         fields = ['id', 'nickname', 'user', 'followee', 'post_set'] # followee: 역참조
+#
+#
+# class ProfilePartSerializer(serializers.ModelSerializer):
+#     user = UserSerializer(read_only=True)
+#
+#     class Meta:
+#         model = Profile
+#         fields = ['id', 'nickname', 'user']
+#
+#
+# class FolloweeListingField2(serializers.RelatedField):
+#     def to_representation(self, value):
+#         return value.nickname
+#
+#
+# class FollowSerializer(serializers.ModelSerializer):
+#     follower = serializers.SerializerMethodField()
+#     followee = FolloweeListingField2(many=True, read_only=True)
+#
+#     def get_follower(self, obj):
+#         return obj.follower.nickname
+#
+#     class Meta:
+#         model = Follow
+#         fields = ['follower', 'followee']
