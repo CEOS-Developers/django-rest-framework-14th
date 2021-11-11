d851f46ff84863fef794654023e3ebd821f54624
ssssujini99 <sssssjin99@naver.com>
2021-11-11 15:53:48 +0900

    CBV기반의 views.py변경완료

--- insta/views.py
+++ insta/views.py
@@ -3,10 +3,9 @@ from django.http import JsonResponse
 
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from .serializers import UserSerializer, PostListSerializer, PostCreateSerializer, FollowSerializer, ProfileListSerializer, ProfilePartSerializer
+from .serializers import UserSerializer, UserCreateSerializer, PostListSerializer, PostCreateSerializer
 
-from .models import Profile, Post, Follow
-from django.contrib.auth.models import User
+from .models import User, Post
 
 ## 추가
 from rest_framework.views import APIView
@@ -16,7 +15,7 @@ from django.shortcuts import get_object_or_404
 
 # Post 모델
 # 1. 전체 Post 가져오기 or # 2. 새 Post 작성하기
-class postListAPIView(APIView):
+class postList(APIView):
     def get(self, request, format=None):
         posts = Post.objects.all()
         serializer = PostListSerializer(posts, many=True)
@@ -31,7 +30,7 @@ class postListAPIView(APIView):
 
 
 # 3. 특정 Post 가져오기 or # 4. 특정 포스트 수정하기 # 5. 특정 포스트 삭제하기
-class postDetailAPIView(APIView):
+class postDetail(APIView):
     def get_object(self, pk):
         return get_object_or_404(Post, id=pk)
 
@@ -54,61 +53,61 @@ class postDetailAPIView(APIView):
         return Response(status=201)
 
 
-# Profile 모델
-# 1. 전체 Profile 가져오기 or # 2. 새 Profile 작성하기 (보류)
-class profileList(APIView):
+# User 모델
+# 1. 전체 User 가져오기 or # 2. 새 User 추가하기 (보류)
+class userList(APIView):
 
     def get(self, request, format=None):
-        profiles = Profile.objects.all()
-        serializer = ProfileListSerializer(profiles, many=True)
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
         return Response(serializer.data)
 
-    # def post(self, request):
-    #     serializer = UserSerializer(data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         내일 해보도록 하겠습니다.. 아무래도 abstractbase user model을 써야할 것 같습니다
+    def post(self, request):
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 
 
 
-# 3. 특정 Profile 가져오기 or # 4. 특정 Profile 수정하기 # 5. 특정 Profile 삭제하기
-class profileDetail(APIView):
+# 3. 특정 User 가져오기 or # 4. 특정 User 수정하기 # 5. 특정 User 삭제하기
+class userDetail(APIView):
     def get_object(self, pk):
-        return get_object_or_404(Profile, id=pk)
+        return get_object_or_404(User, id=pk)
 
     def get(self, request, pk, format=None):
-        profile = Profile.objects.get(id=pk)
-        serializer = ProfilePartSerializer(profile)
+        user = User.objects.get(id=pk)
+        serializer = UserSerializer(user)
         return Response(serializer.data)
 
     def put(self, request, pk):
-        Profile = self.get_object(pk)
-        serializer = ProfilePartSerializer(Profile, data=request.data)
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
     def delete(self, request, pk):
-        profile = self.get_object(pk)
-        profile.delete()
         user = User.objects.get(id=pk)
         user.delete()
         return Response(status=204)
 
 
-# # Follow 모델
-# # 1. 전체 팔로우 관계 조회하기
-class followList(APIView):
-    def get(self, request, format=None):
-        follows = Follow.objects.all()
-        serializer = FollowSerializer(follows, many=True)
-        return Response(serializer.data)
-
-
-# # 2. 특정 유저의 팔로우 관계 조회하기
-class followDetail(APIView):
-    def get(self, request, pk, format=None):
-        follow = Follow.objects.get(follower=pk)
-        serializer = FollowSerializer(follow)
-        return Response(serializer.data)
+# # # Follow 모델
+# # # 1. 전체 팔로우 관계 조회하기
+# class followList(APIView):
+#     def get(self, request, format=None):
+#         follows = Follow.objects.all()
+#         serializer = FollowSerializer(follows, many=True)
+#         return Response(serializer.data)
+#
+#
+# # # 2. 특정 유저의 팔로우 관계 조회하기
+# class followDetail(APIView):
+#     def get(self, request, pk, format=None):
+#         follow = Follow.objects.get(follower=pk)
+#         serializer = FollowSerializer(follow)
+#         return Response(serializer.data)
