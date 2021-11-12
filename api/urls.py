82dd32ffcf376909f052cb6b5e0ec89dc914b349
hywn <14hhan@gmail.com>
2021-11-13 01:55:37 +0900

    feat: APIView CBV 기반 리팩토링 및 Post 업데이트, 삭제 API 구현

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('profile/<int:profile_id>/', views.profile_detail),
-    path('posts/', views.post_list),
-    path('post/<int:post_id>/', views.post_detail),
-    path('comments/', views.comment_create),
-    path('comment/<int:comment_id>/', views.comment_detail),
-    path('likes/', views.like_create)
+    path('profile/<int:pk>/', views.ProfileDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
+    path('comments/', views.CommentDetail.as_view()),
+    path('comment/<int:pk>/', views.CommentDetail.as_view()),
+    path('likes/', views.LikeDetail.as_view())
 ]
