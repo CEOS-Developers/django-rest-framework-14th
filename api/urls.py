60e27e620bf114bb0f4fb734e3f60d17a04b1c49
mongus <kjyanus@gmail.com>
2021-11-17 01:59:04 +0900

    using basic ViewSet; define actions manually

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet)
+router.register(r'posts', PostViewSet, basename='post')
 
 urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,12 +1,18 @@
 from rest_framework.response import Response
-from rest_framework import status
 from rest_framework.generics import get_object_or_404
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins, status
 
 from .models import *
 from .serializers import PostSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    serializer_class = PostSerializer
-    queryset = Post.objects.all()
+class PostViewSet(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def retrieve(self, request, pk=None):
+        post = get_object_or_404(Post, pk=pk)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
