2fd4a32cfd8c32b402e12c0d34c497621077de6f
mongus <kjyanus@gmail.com>
2021-11-17 02:24:33 +0900

    custom ModelViewSet w/ action decorator

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from rest_framework import routers
 from .views import PostViewSet
 
 router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts', PostViewSet)
 
 urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -1,18 +1,23 @@
 from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework import viewsets, mixins, status
-
+from rest_framework.decorators import action
 from .models import *
 from .serializers import PostSerializer
 
 
-class PostViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Post.objects.all()
-        serializer = PostSerializer(queryset, many=True)
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    # /api/posts/reversed_id
+    @action(detail=False)
+    def reversedid(self, request):
+        reversed_posts = Post.objects.all().order_by("-id")
+        serializer = PostSerializer(reversed_posts, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
-        post = get_object_or_404(Post, pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+    # /api/posts/1/customDetailFunction
+    @action(detail=True)
+    def customDetailFunction(self, request, pk=None):
+        return Response({"data": "커스텀한 정보"})
