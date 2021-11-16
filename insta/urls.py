80cfdde5a4e01b0bf2c657efdc1ef67a310cf259
ssssujini99 <sssssjin99@naver.com>
2021-11-17 02:28:36 +0900

    urls 추가 수정

--- insta/urls.py
+++ insta/urls.py
@@ -3,12 +3,13 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('users/', views.userList.as_view()),
-    path('users/<int:pk>/', views.userDetail.as_view()),
+    path('users', views.userList.as_view()),
+    path('users/<int:pk>', views.userDetail.as_view()),
     #
-    path('posts/', views.postList.as_view()),
-    path('posts/<int:pk>/', views.postDetail.as_view()),
+    path('posts', views.postList.as_view()),
+    path('posts/<int:pk>', views.postDetail.as_view()),
     #
-    # path('follow/', views.followList.as_view()),
-    # path('follow/<str:pk>/', views.followDetail.as_view()),
+    path('follow', views.followList.as_view()),
+    path('following/<int:pk>', views.following.as_view()),
+    path('followee/<int:pk>', views.followee.as_view()),
 ]
