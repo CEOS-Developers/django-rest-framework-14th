eaa938b36d72c8d9ac492517f7f47ca2d00d87e5
chaeri93 <chaeri93@ewhain.net>
2021-11-16 17:32:13 +0900

    add ViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,1 @@
-from django.urls import path
-from api import views
-from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>', views.PostDetail.as_view())
-]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -1,44 +1,15 @@
 from django.http import Http404
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from api.models import Post
-from api.serializers import PostSerializer
+from api.models import Post, Profile
+from api.serializers import PostSerializer, ProfileSerializer
 
 
-class PostList(APIView):
-    def get(self, request, format=None): # Post 전체 가지고 오기
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
 
-    def post(self, request, format=None): # Post 작성하기
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-class PostDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-    def delete(self, request, pk, format=None): # 특정 Post 삭제
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=204)
+class ProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
