df4fdece670e9ad70d32ef0477404de11dd37727
YOONJI KIM <77983074+stritegdc@users.noreply.github.com>
2021-11-16 16:05:52 +0900

    Add basic PostViewSet, UserViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 from api import views
+from .views import PostViewSet, UserViewSet
+from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path('posts/', views.PostList.as_view(), name='posts'),
-    path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
-    path('users/', views.UserList.as_view(), name='users'),
-    path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
-]
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'users', UserViewSet)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('posts/', views.PostList.as_view(), name='posts'),
+#     path('posts/<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+#     path('users/', views.UserList.as_view(), name='users'),
+#     path('users/<int:pk>', views.UserDetail.as_view(), name='user_detail'),
+# ]
+#
+# urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -4,89 +4,100 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
+from rest_framework import viewsets
 
 # Create your views here.
 
 
-class PostList(APIView):
-    """
-      View to List all posts, or create a new post.
-    """
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
 
-class PostDetail(APIView):
-    """
-        View to detail post
-    """
-    def get_object(self, pk):
-        post = get_object_or_404(Post, pk=pk)
-        return post
-
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk=pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk=pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk=pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class UserList(APIView):
-    def get(self, request, format=None):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserDetail(APIView):
-    def get_object(self, pk):
-        user = get_object_or_404(User, pk=pk)
-        return user
-
-    def get(self, request, pk, format=None):
-        user = self.get_object(pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        user = self.get_object(pk=pk)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        user = self.get_object(pk=pk)
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class PostList(APIView):
+#     """
+#       View to List all posts, or create a new post.
+#     """
+#     def get(self, request, format=None):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class PostDetail(APIView):
+#     """
+#         View to detail post
+#     """
+#     def get_object(self, pk):
+#         post = get_object_or_404(Post, pk=pk)
+#         return post
+#
+#     def get(self, request, pk, format=None):
+#         post = self.get_object(pk=pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         post = self.get_object(pk=pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         post = self.get_object(pk=pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+#
+#
+# class UserList(APIView):
+#     def get(self, request, format=None):
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class UserDetail(APIView):
+#     def get_object(self, pk):
+#         user = get_object_or_404(User, pk=pk)
+#         return user
+#
+#     def get(self, request, pk, format=None):
+#         user = self.get_object(pk=pk)
+#         serializer = UserSerializer(user)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         user = self.get_object(pk=pk)
+#         serializer = UserSerializer(user, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         user = self.get_object(pk=pk)
+#         user.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
