0320c1adb5e774910a5ef9c8fc9d716c4fa7e440
chaeri93 <chaeri93@ewhain.net>
2021-11-16 17:32:59 +0900

    add Router to urls,py

--- api/urls.py
+++ api/urls.py
@@ -1,1 +1,9 @@
 
+from rest_framework import routers
+from api.views import PostViewSet, ProfileViewSet
+
+router = routers.DefaultRouter()
+router.register(r'posts', PostViewSet, basname='post')
+router.register(r'users', ProfileViewSet, basename='users')
+
+urlpatterns = router.urls
