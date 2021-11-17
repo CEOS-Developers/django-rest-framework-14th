d3ada2e3838804730db29f757942cc662b3b0a76
Min A <min2010011@gmail.com>
2021-11-17 23:15:04 +0900

    refactor(view): APIView를 ModelViewSet으로 변경

--- api/views.py
+++ api/views.py
@@ -1,55 +1,11 @@
-from django.http import Http404
-
 from .models import Post
 from .serializers import PostSerializer
-from rest_framework import viewsets, status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-# class PostViewSet(viewsets.ModelViewSet):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#
-#     # create() 재정의
-#     def perform_create(self, serializer):
-#         serializer.save(author=self.request.user)
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-
-class PostList(APIView):
-    def get(self, request, format=None):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(author=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class PostDetail(APIView):
-    def get_post(self, pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_post(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # create() 재정의
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
