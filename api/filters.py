fcba1e0a9ab6fd29afb285f8ba32965ace999f1f
YOONJI KIM <77983074+stritegdc@users.noreply.github.com>
2021-11-17 17:13:25 +0900

    Add info with email filtering to UserFilter

--- api/filters.py
+++ api/filters.py
@@ -2,6 +2,7 @@ from django.contrib.admin.utils import lookup_field
 from django_filters.rest_framework import FilterSet, filters
 from rest_framework.generics import get_object_or_404
 from .models import *
+import re
 
 
 class PostFilter(FilterSet):
@@ -13,7 +14,7 @@ class PostFilter(FilterSet):
             'created_date': ['lt', 'gt']
         }
 
-    # user가 팔로우하는 계정의 post만 가져오도록.
+    # user가 팔로우하는 계정의 post만 가져오도록 filtering.
     def filter_following_posts(self, queryset, name, value):
         user = get_object_or_404(User, pk=value)
         filtered_posts = queryset.filter(author__followers__follower=user)
@@ -21,11 +22,28 @@ class PostFilter(FilterSet):
 
 
 class UserFilter(FilterSet):
-    nickname = filters.CharFilter(field_name='nickname', lookup_expr='icontains')
+    # info에 email 정보를 써둔 계정만 filtering.
+    info = filters.BooleanFilter(method='filter_has_email', label='has_email')
 
     class Meta:
         model = User
-        fields = ['nickname']
+        fields = ['profile']
+
+    def filter_has_email(self, queryset, name, value):
+        pattern = r"([\w\.-]+)@([\w\.-]+)(\.[\w\.]+)"
+        user_with_email = []
+        user_without_email = []
+        for user in queryset:
+            match = re.search(pattern, user.profile.info)
+            if match:
+                user_with_email.append(user.profile)
+            else:
+                user_without_email.append(user.profile)
+
+        return queryset.filter(profile__in=user_with_email)
+
+
+
 
 
 
