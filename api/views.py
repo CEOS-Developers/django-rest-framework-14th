7fff27de570b6a0fdba10dc10130a49c44dc90b7
chaeri93 <chaeri93@ewhain.net>
2021-11-18 15:55:39 +0900

    add filterset

--- api/views.py
+++ api/views.py
@@ -1,14 +1,39 @@
-from django.http import Http404
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from api.models import Post, Profile
 from api.serializers import PostSerializer, ProfileSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from django_filters.rest_framework import FilterSet, filters
+
+
+class PostFilter(FilterSet):
+    content = filters.CharFilter(field_name='content')
+    content_null = filters.BooleanFilter(field_name='content', method='is_content_null')
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def is_content_null(self,queryset,content,value):
+        if value:
+            return queryset.filter(content__isnull=True)
+        else:
+            return queryset.filter(content__isnull=False)
+
+
+class ProfileFilter(FilterSet):
+    nickname = filters.CharFilter(field_name='nickname')
+
+    class Meta:
+        model = Profile
+        fields =['nickname']
 
 
 class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filter_class = PostFilter
+
 
 class ProfileViewSet(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
